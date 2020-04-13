0d196726283c954bcbcbcaa6b0aa8d833df6d362
Claudio <claudio.moya@rakuten.com>
2020-04-13 13:27:41 +0900

    Update - Refactoring of Home links

--- rex-app/src/Home.jsx
+++ rex-app/src/Home.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import { Route, Switch as RouterSwitch, Link as RouterLink } from 'react-router-dom';
 
 export default function Home() {
+  const pages = [
+    'Button',
+    'Checkbox',
+    'Grid',
+    'Paper',
+    'Password',
+    'RadioButton',
+    'Select',
+    'Switch',
+    'TextField',
+    'Textarea',
+    'Track',
+  ];
+
   return (
     <RouterSwitch>
       <Route exact path="/">
@@ -9,42 +23,15 @@ export default function Home() {
           <h1>Home</h1>
           <h2>Forms</h2>
           <ul>
-            <li>
-              <RouterLink to="/Button">Button</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Checkbox">Checkbox</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/RadioButton">RadioButton</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Track">Track</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Switch">Switch</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Stepper">Stepper</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Select">Select</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Paper">Paper</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Textarea">Textarea</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/TextField">TextField</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Grid">Grid</RouterLink>
-            </li>
-            <li>
-              <RouterLink to="/Password">Password</RouterLink>
-            </li>
+            {
+              pages.map(pageName => {
+                return (
+                  <li>
+                    <RouterLink to={`/${pageName}`}>{pageName}</RouterLink>
+                  </li>
+                );
+              })
+            }
           </ul>
         </div>
       </Route>
